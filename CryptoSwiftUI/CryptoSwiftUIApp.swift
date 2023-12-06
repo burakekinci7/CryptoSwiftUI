bab753c3209f571d36c0b452252f6abde62b462b
burakekinci7 <ekincib015@gmail.com>
2023-12-06 12:26:01 +0300

    Initial Commit

---
+++ CryptoSwiftUI/CryptoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftUIApp.swift
+//  CryptoSwiftUI
+//
+//  Created by Ramazan Burak Ekinci on 6.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
